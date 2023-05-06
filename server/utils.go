3e4edbc6d585ceb4e436705f49411fd9490a7034
Karthika Rajagopal <78952344+Karthika-Rajagopal@users.noreply.github.com>
2023-05-06 14:11:19 +0530

    Update utils.go

--- server/utils.go
+++ server/utils.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func getEvent(w http.ResponseWriter, req * http.Request) (eventRes * stripe.Event, err error){
+func getEvent(w http.ResponseWriter, req * http.Request) (eventRes * stripe.Event, err error){  //getEvent(). This function takes an HTTP request as input and reads the request body up to a maximum size of 65536 bytes. It then reads the payload and unmarshals it into a stripe.Event struct using the json.Unmarshal() function
 	const MaxBodyBytes = int64(65536)
 	req.Body = http.MaxBytesReader(w, req.Body, MaxBodyBytes)
 	payload, err := ioutil.ReadAll(req.Body)
@@ -25,7 +25,7 @@ func getEvent(w http.ResponseWriter, req * http.Request) (eventRes * stripe.Even
 
 
 
-func CORSCheck(handler func (w http.ResponseWriter, req *http.Request)) func (w http.ResponseWriter, req *http.Request){
+func CORSCheck(handler func (w http.ResponseWriter, req *http.Request)) func (w http.ResponseWriter, req *http.Request){  //This function takes an HTTP handler function as input and returns a new handler function with CORS headers set. The CORS headers are used to allow cross-origin requests to the API. The returned handler function checks if the HTTP method is OPTIONS
 	res := func (w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
